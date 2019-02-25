315cd8271428106f8e5eb0d1a97bd12cdbb92efa
JaimeNufio <jaimeenufio@gmail.com>
2019-02-25 09:32:39 -0500

    Started on JSON creation buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var php = "https://web.njit.edu/~jen25/index.php"
 var result = document.getElementById("result");
 var result1 = document.getElementById("result1");
+var count = 0;
 
 function POST(args){
 	var xhttp = new XMLHttpRequest();
@@ -33,15 +34,17 @@ function login(){
 	//document.getElementById("result").innerHTML=args;
 	POST(args);
 }
-	
+
+
 function createJSONQuestionAdd(){
+	let id = document.getElementById("fname").value+"__"+String(count);
   let question = {
-		"func_ID" : {
+		id : {
 			"func_name" : document.getElementById("fname").value,
 			"arg_names" : document.getElementById("args").value.split(","),
 			"description" : document.getElementById("desc").value,
-			"inputs" : [document.getElementById("in1").value, document.getElementById("in2").value],
-			"expected_outputs" : [document.getElementById("out1").value, document.getElementById("out2").value],
+			"inputs" : [document.getElementById("in1").value.split(","), document.getElementById("in2").value.split(",")],
+			"expected_outputs" : [document.getElementById("out1"), document.getElementById("out2")],
 			"difficulty" : document.getElementById("difficulty").value,
 			"topic" : document.getElementById("topic").value
 	}
@@ -52,4 +55,19 @@ function createJSONQuestionAdd(){
   console.log(question);
 }
 
+//This function is called every update on the search, hopefully rewritting the divs won't be a problem?
+function createJSONQuestionQuery(){
+	//For Matt, tell him that if the querry['x'] is empty, skip this search.
+	let query = {
+		"topic" : document.getElementById("qtopic").value,
+		"keywords": document.getElementById("qkeywords").value.split(","),
+		"difficulty": document.getElementById("qdifficulty").value 
+	}
+
+	console.log(query)
+}
+
 createJSONQuestionAdd();																																																															  //
+//TODO
+//get all questions on page load (Will affect load times?)
+
