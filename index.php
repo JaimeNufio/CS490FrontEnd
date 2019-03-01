7e16bb3bbb042937613db4e649a815bd832b5c01
JaimeNufio <jaimeenufio@gmail.com>
2019-03-01 00:04:17 -0500

    JS for teacher appears 90%

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+
+//echo "User:",$_POST['user_name'],"\nPass:",$_POST['password'];
+//print_r($_GET)
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$middle = 'https://web.njit.edu/~jen25/dummy.php';
+	//$middle = 'https://web.njit.edu/~sn479/index.php';
+
+  $array = [
+    $_POST['user_name'],
+    $_POST['password']
+  ]
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => $middle,
+		CURLOPT_POST => 1,
+		CURLOPT_POSTFIELDS => array(
+			request_type => 'login', 
+			data =>   
+		)
+	));
+
+
+	$result = curl_exec($curl);
+	curl_close($curl);
+	echo $result;
+}else{
+	echo "nouser";
+}
--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-
 var jaime = "https://web.njit.edu/~jen25/handle.php"
 var scott = "https://web.njit.edu/~sn479/index.php"
-var dummy = "https://web.njit.edu/~jen25/dummy.php"
+var php = "https://web.njit.edu/~jen25/dummy.php"
+
+if (document.addEventListener) {
+    document.addEventListener("click", handleClick, false);
+}
+else if (document.attachEvent) {
+    document.attachEvent("onclick", handleClick);
+}
+
+var absoluteQNum = 0;
 var result = document.getElementById("result");
 var result1 = document.getElementById("result1");
-var count = 0;
+var total = 0; //pts of test
+var qNum = 0;
 
 //theObject will be assigned to the value of var['questions']
 var theObject = {
@@ -150,29 +159,31 @@ var theObject = {
 	}
 }
 
+var functionList = [];
+
 function login(){
 
 	result1.innerHTML="";
 	result.innerHTML="";
-        
+
 	let args = {
 		'username':""+document.getElementById("user").value,
 		'password':""+document.getElementById("pass").value
 	};
 	console.log(args);
-	
+
 	//"user_name="+document.getElementById("user").value+"&password="+document.getElementById("pass").value;
 	//document.getElementById("result").innerHTML=args;
 	var xhttp = new XMLHttpRequest();
 
   	xhttp.onreadystatechange = function() {
-	
+
 	if (this.readyState == 4 && this.status == 200) {
 		if (this.response == "instructor" || this.response == "student"){
 			result.style.color="#31c55a";
 			result.innerHTML+="Login valid, Redirecting.\n";
 			if (this.response == "instructor"){
-				window.location.href = "/teacherSearch.html";	
+				window.location.href = "/teacherSearch.html";
 			}
 		}else{
 			result.style.color="#e40042";
@@ -189,37 +200,109 @@ function login(){
 	console.log(xhttp);
 }
 
-function createJSONQuestionAdd(){
-	let id = document.getElementById("fname").value+"__"+String(count);
-  	let question = {
-		(""+id) : {
-			"func_name" : document.getElementById("fname").value,
-			"arg_names" : document.getElementById("vars").value.split(","),
-			"description" : document.getElementById("desc").value,
-			"inputs" : [document.getElementById("in1").value.split(","), document.getElementById("in2").value.split(",")],
-			"expected_outputs" : [document.getElementById("out1"), document.getElementById("out2")],
-			"difficulty" : document.getElementById("difficulty").value,
-			"topic" : document.getElementById("topic").value
+
+function handleClick(event) {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    var element = event.target;
+
+    // Climb up the document tree from the target of the event
+    while (element) {
+        if (element.nodeName === "BUTTON" && /questionPreviewButton/.test(element.className)) {
+            ButtonStateHandler(element);
+            break;
+        }
+
+        element = element.parentNode;
+    }
+}
+
+function ButtonStateHandler(button) {
+	let val = (button.parentElement.getElementsByClassName('select')[0].value);
+
+	if (button.classList.contains("up")){
+		let temp =total-(-val);
+		if (!isNaN(val) && val > 0 && temp<=100){
+
+			let buttonId = button.parentElement.parentElement.parentElement.parentElement.id;
+
+			total-=-val;
+			button.parentElement.getElementsByClassName('select')[0].readOnly = true;
+			button.classList.add("down");
+			button.classList.remove("up");
+			button.innerHTML = "Remove";
+
+			let key = Object.keys(theObject)[buttonId];
+			let obj = theObject[key];
+
+			console.log(Object.keys(theObject)[buttonId]);
+			//console.log(button.parentElement.parentElement.parentElement.parentElement.id);
+			functionList.push(Object.keys(theObject)[buttonId]);
+
+		}else{
+			console.log("NaN");
+		}
+	}else{
+		let buttonId = button.parentElement.parentElement.parentElement.parentElement.id;
+
+		button.classList.remove("down");
+		button.classList.add("up");
+		total-=-(-val);
+		button.parentElement.getElementsByClassName('select')[0].readOnly = false;
+		button.innerHTML = "Append";
+
+		console.log(Object.keys(theObject)[buttonId]);
+
+		for (var i =0; i < functionList.length;i++){
+			if (functionList[i]===Object.keys(theObject)[buttonId]){
+				functionList.splice(i,1);
+			}
 		}
 	}
+	document.getElementById("total").innerHTML=`${total}/100`;
+	console.log(functionList);
+}
 
-	var xhttp = new XMLHttpRequest();
+function createJSONQuestionAdd(){
 
-  	xhttp.onreadystatechange = function() {
-	
-		if (this.readyState == 4 && this.status == 200) {
+	if (document.getElementById("fname").value.length == 0 || document.getElementById("vars").length == 0 ||
+	document.getElementById("desc").length == 0 || document.getElementById("in1").length == 0 ||
+	document.getElementById("out1").length == 0 || document.getElementById("topic").length == 0 ||
+	document.getElementById("in2").length == 0 || document.getElementById("out2").length == 0 ){
+		console.log("Something is empty...");
+	}else{
+		let id = document.getElementById("fname").value+"__"+String(absoluteQNum);
+		let question = {
+			id : {
+				"func_name" : document.getElementById("fname").value,
+				"arg_names" : document.getElementById("vars").value.split(","),
+				"description" : document.getElementById("desc").value,
+				"inputs" : [document.getElementById("in1").value.split(","), document.getElementById("in2").value.split(",")],
+				"expected_outputs" : [document.getElementById("out1"), document.getElementById("out2")],
+				"difficulty" : document.getElementById("difficulty").value,
+				"topic" : document.getElementById("topic").value
+			}
 		}
 
-	};
+		var xhttp = new XMLHttpRequest();
 
-	xhttp.open("POST", scott, true);
-	xhttp.setRequestHeader("Request_Type", "new_question");
-	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhttp.send(JSON.stringify(question));
-	console.log(xhttp);
+		xhttp.onreadystatechange = function() {
 
-//Request to post this question
-	createJSONQuestionQuery();
+			if (this.readyState == 4 && this.status == 200) {
+			}
+
+		};
+
+		xhttp.open("POST", scott, true);
+		xhttp.setRequestHeader("Request_Type", "new_question");
+		xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhttp.send(JSON.stringify(question));
+		console.log(xhttp);
+
+		//Update the search bar, new question might appear.
+		createJSONQuestionQuery();
+	}
 }
 
 //This function is called every update on the search, hopefully rewritting the divs won't be a problem?
@@ -232,30 +315,88 @@ function createJSONQuestionQuery(){
 		"difficulty": document.getElementById("qdifficulty").value
 	}
 
-	console.log(query)
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() {
+
+		if (this.readyState == 4 && this.status == 200) {
+			theObject = JSON.decode(response);
+			CreateListForTeacher(); //Redraw the list based on new Query
+		}
+
+  	};
+
+	xhttp.open("POST", scott, true);
+	xhttp.setRequestHeader("Request_Type", "query");
+	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhttp.send(JSON.stringify(query));
+	console.log(xhttp);
 }
 
-function buildTextUnit(topic, keywords, desc, difficulty){
+function createAbsoluteNumQuery(){
+	var xhttp = new XMLHttpRequest();
 
-	return `<div class="questionPreview">
+	let args = {};
+  	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			absoluteQNum = this.response;
+		}
+	};
+
+	xhttp.open("POST", scott, true);
+	xhttp.setRequestHeader("Request_Type", "num_questions");
+	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhttp.send(JSON.stringify(args));
+	console.log(xhttp);
+}
+
+function buildTextUnit(topic, func_name, desc, difficulty){
+	return `<div class="questionPreview" id = ${qNum}>
+	<div class="questionPreviewTitle">ID: ${func_name} TOPIC: ${topic}</div>
+	<hr>
+	<div class="questionPreviewText">
+		<span class="smallHeading">Question:</span>
+	 	<br>
+		<div>
+			${desc}
+		</div>
+	<hr>
+	<div class="questionPreviewHandler">
+	  <div style="margin-top:8px;" class="questionPreviewContainer">
+		<div style="margin-top:8px; margin-right:5px; margin-left:5px" class="questionPreviewDifficulty">Difficulty: ${difficulty}</div>
+		<div style="float:left;margin-top:8px; margin-right:5px">Points Value:</div>
+		<input class="select pts" style="height:15px;float:left;width:10%;margin-top:5px"></input>
+		<button style="float:right; margin-right:20px; width:20%;" class="up questionPreviewButton">Append</button>
+	  </div>
+	</div>
+  </div>`;
+
+
+	/*`<div class="questionPreview">
 	<div class="questionPreviewTitle">TOPIC: ${topic} KEYWORDS: ${keywords}</div>
 	<hr>
 	<div class="questionPreviewText">	${desc}</div>
 	<hr>
 	<div class = "questionPreviewDifficulty">Difficulty: ${difficulty}</div>
-	</div>`
+	</div>`*/
 
 }
 
-function insert(){
+function CreateListForTeacher(){
+
 	var scroll = document.getElementById("heap");
+	scroll.innerHTML = ""; //Erase the current.
+
+	qNum = 0;
 
 	for (let i = 0; i<Object.keys(theObject).length;i++){
 		let key = Object.keys(theObject)[i];
 		let obj = theObject[key]['topic']
-		console.log([obj])
-		let piece = buildTextUnit(theObject[key]['topic'],arrayToWords(theObject[key]['keywords']),theObject[key]['description'],theObject[key]['difficulty']);
+		console.log("key: "+key)
+		//console.log([obj])
+		let piece = buildTextUnit(theObject[key]['topic'],key,theObject[key]['description'],theObject[key]['difficulty']);
 		scroll.innerHTML+=piece;
+		qNum++;
 	}
 }
 
@@ -271,11 +412,10 @@ function arrayToWords(l){
 	return out;
 }
 
-//Initially Load Everything in DB
-createJSONQuestionQuery();
-
-
-//insert();																																																						  //
+//On Load, we're required to know the # of functions in the DB
+createAbsoluteNumQuery()
+//Create a list of questions for the teacher to skim
+CreateListForTeacher();																																																						  //
 
 //TODO
 //get all questions on page load (Will affect load times?)
